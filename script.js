b00099bfac3d3ccfebf19a750b500c3b17f05607
sawgur <107877277+sawgur@users.noreply.github.com>
2023-05-21 17:59:36 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const animatedElements = document.querySelectorAll('.animate');
+
+  function checkViewport() {
+    for (let i = 0; i < animatedElements.length; i++) {
+      const element = animatedElements[i];
+      const positionFromTop = element.getBoundingClientRect().top;
+
+      if (positionFromTop - window.innerHeight <= 0) {
+        element.classList.add('animate-visible');
+      }
+    }
+  }
+
+  function scrollHandler() {
+    checkViewport();
+    window.addEventListener('scroll', checkViewport);
+  }
+
+  scrollHandler();
+});
